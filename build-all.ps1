3d1e88ca1a976f3c5e4558de2d30621a014543c2
Keiaa <confinedrose@duck.com>
2025-08-17 09:44:41 +0800

    Ongoing cleanup. Added MPL headers and started tracking .txt files which may or may not include specific licenses included herein.

--- build-all.ps1
+++ build-all.ps1
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 # GOMIS Project Root Build Script
 # Calls the main build script in gomis-installer, passing all arguments
 
