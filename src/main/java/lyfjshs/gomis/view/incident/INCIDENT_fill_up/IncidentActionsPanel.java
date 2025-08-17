0b934c1b432bb9034861b90efca1409a6a7f3bcd
Keiaa <confinedrose@duck.com>
2025-08-17 10:09:41 +0800

    Added MPL headers to lyfjshs.gomis.view.incident.INCIDENT_fill_up.

--- src/main/java/lyfjshs/gomis/view/incident/INCIDENT_fill_up/IncidentActionsPanel.java
+++ src/main/java/lyfjshs/gomis/view/incident/INCIDENT_fill_up/IncidentActionsPanel.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package lyfjshs.gomis.view.incident.INCIDENT_fill_up;
 
 import javax.swing.JLabel;
